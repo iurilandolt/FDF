b58391416a8e5d10c928ce532b2695b376c2dc0c
Rubem Iuri Landolt Vieira Da Silva <rlandolt@c1r9s2.42lisboa.com>
2024-01-10 12:27:32 +0000

    project done, cleaning up

--- .resources/minilibx_intro.c
+++ .resources/minilibx_intro.c
@@ -4,32 +4,6 @@
 #define WIDTH 1280
 #define HEIGHT 720
 
-
-void draw_circle(void *mlx_server, void *mlx_win, int cx, int cy, int radius)
-{
-	for (int y = cy - radius; y <= cy + radius; ++y)
-	{
-		for (int x = cx - radius; x <= cx + radius; ++x)
-		{
-			if ((x - cx) * (x - cx) + (y - cy) * (y - cy) <= radius * radius)
-			{
-				mlx_pixel_put(mlx_server, mlx_win, x, y, rand() % 0x1000000);
-			}
-		}
-	}
-}
-
-void draw_rectangle(void *mlx_server, void *mlx_win, int width, int height)
-{
-	for (int y = HEIGHT * 0.1; y < height * 0.9; ++y)
-	{
-		for (int x = WIDTH * 0.1; x < width * 0.9; ++x)
-		{
-			mlx_pixel_put(mlx_server, mlx_win, x, y, rand() % 0x1000000);
-		}
-	}
-}
-
 int	main()
 {
 	void	*mlx_server;
@@ -87,4 +61,76 @@ void draw_map_recursive(t_session *instance, int x, int y) {
 
 void draw_map(t_session *instance) {
     draw_map_recursive(instance, 0, 0);
+}
+
+
+draw_circle(&instance->mlx_img, WIDTH / 2, HEIGHT /2, 500, 0x00FF0000);
+void draw_circle(t_data *data, int cx, int cy, int radius, int color)
+{
+	for (int y = cy - radius; y <= cy + radius; ++y)
+	{
+		for (int x = cx - radius; x <= cx + radius; ++x)
+		{
+			if ((x - cx) * (x - cx) + (y - cy) * (y - cy) <= radius * radius)
+			{
+				my_mlx_pixel_put(data, x, y, color);
+			}
+		}
+	}
+}
+
+void draw_rectangle(t_data *data, int height, int width, int color)
+{
+	for (int y = 0; y < height; ++y)
+	{
+		for (int x = 0; x < width; ++x)
+		{
+			my_mlx_pixel_put(data, x, y, color);
+		}
+	}
+}
+
+void draw_array_cells(t_data *data, int **array, int rows, int cols)
+{
+	int width = W_WIDTH / cols;   // Calculate width of each rectangle
+	int height = W_HEIGHT / rows; // Calculate height of each rectangle
+
+	for (int y = 0; y < rows; ++y)
+	{
+		for (int x = 0; x < cols; ++x)
+		{
+			int color = array[y][x] * 10;
+			int startX = x * width;
+			int startY = y * height;
+
+			// Draw a point for each cell // d stands for displacement
+			for (int dy = 0; dy < height; ++dy)
+			{
+				for (int dx = 0; dx < width; ++dx)
+				{
+					my_mlx_pixel_put(data, startX + dx, startY + dy, color);
+				}
+			}
+		}
+	}
+}
+
+void draw_array_points(t_data *data, int **array, int rows, int cols)
+{
+	// Calculate the size of each square
+	int dx = W_WIDTH / cols;
+	int dy = W_HEIGHT / rows;
+
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < cols; ++j)
+		{
+			int color = array[i][j] * 25;
+			int x_center = j * dx + dx / 2; // Calculate the center x-coordinate
+			int y_center = i * dy + dy / 2; // Calculate the center y-coordinate
+
+			// Draw a single point for each cell
+			my_mlx_pixel_put(data, y_center, x_center, color);
+		}
+	}
 }
